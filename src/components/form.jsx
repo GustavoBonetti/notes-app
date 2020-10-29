efeb7cfaea3af3d2f2314c68647f996e39d92434
Gustavo Bonetti <gustavobonetti8@gmail.com>
2020-10-29 10:12:27 -0300

    install lint and add form

---
+++ src/components/form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Form = () => (
+  <form>
+    <input type="text" placeholder="Título" />
+    <textarea placeholder="Escreva sua nota..." />
+    <button type="submit">Criar nota</button>
+  </form>
+);
+
+export default Form;
